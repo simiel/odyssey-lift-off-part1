b5c4a0458c35d8d6bd9df95afb6e4e5d3da92dec
Simiel Adjei <smenxah@gmail.com>
2025-02-13 12:44:21 +0000

    set up server & mock data

---
+++ server/src/schema.ts
@@ -0,0 +1,24 @@
+import { gql } from "graphql-tag";
+
+export const typeDefs = gql`
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+`;
